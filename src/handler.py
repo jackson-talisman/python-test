c26e474ebeec0b80d1d8e7e2fe0a96cf4c77e6b0
jackson-talisman <jackson@gettalisman.com>
2025-02-04 10:48:53 -0600

    testing pipeline

--- src/handler.py
+++ src/handler.py
@@ -3,4 +3,8 @@ def handler(event, context):
     # Log the event argument for debugging and for use in local development.
     print(json.dumps(event))
 
-    return {}
+    # Return a response to the event caller.
+    response = {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
